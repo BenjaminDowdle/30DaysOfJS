d29a79511ed6ee9977f783328b8dbb5480ef2bd6
BenjaminDowdle <benjamin.dowdle99@gmail.com>
2023-04-18 16:16:26 -0600

    Day 3 Practice

--- day3/src/index.js
+++ day3/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Header = () => (
+  <h1>Hello World!</h1>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Main = () => (
+  <h2>Benjamin Dowdle</h2>
+)
+
+const App = () => (
+  <div className='app'>
+    <Header />
+    <Main />
+  </div>
+)
+
+const root = document.getElementById('root');
+
+ReactDOM.render(<App />, root)
